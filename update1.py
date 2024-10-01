33b2eec71ba4f096f5120c73ccf4092c7992c87a
GIANG NGUYỄN <hoangiang272003@gmail.com>
2024-10-01 16:21:41 +0700

    connect with database

--- update1.py
+++ update1.py
@@ -82,7 +82,26 @@ class Face_Recognizer:
         #  Reclassify after 'reclassify_interval' frames
         self.reclassify_interval_cnt = 0
         self.reclassify_interval = 10
-
+    def connect_to_database(self):
+        # """connect with database MySQL"""
+        try:
+            connection = mysql.connector.connect(
+                host='127.0.0.1',  # address host of MySQL
+                database='studentdb',  # name of database
+                user='user',  # Username of MySQL
+                password='user',  # Password of MySQL
+                port='3309',
+                raise_on_warnings=True  # keep informed if there's error 
+            )
+            # Database connection configuration
+             # Database connection configuration
+            if connection.is_connected():
+                print("Success! Connected to MySQL database.")
+                return connection
+        except Error as e:
+            print(f"fail to connect with MySQL database: {e}")
+            return None
+        
     #  "features_all.csv"  / Get known faces from "features_all.csv"
     def get_face_database(self):
         if os.path.exists("data/features_all.csv"):
