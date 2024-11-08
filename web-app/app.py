9a21b67468117346d22e3dd155c17d6a5e226388
GIANG NGUYỄN <hoangiang272003@gmail.com>
2024-11-08 12:37:22 +0700

    decorate web page

--- web-app/app.py
+++ web-app/app.py
@@ -1,28 +1,25 @@
 from flask import Flask, render_template, request
-import sqlite3
-from datetime import datetime
 import mysql.connector
 from mysql.connector import Error
 
-def connect_to_database():
-        # """connect with database MySQL"""
-        try:
-            connection = mysql.connector.connect(
-                host='127.0.0.1',  # address host of MySQL
-                database='studentdb',  # name of database
-                user='user',  # Username of MySQL
-                password='user',  # Password of MySQL
-                port='3309',
-                raise_on_warnings=True  # keep informed if there's error 
-            )
-            # Database connection configuration
-            if connection.is_connected():
-                print("Success! Connected to MySQL database.")
-                return connection
-        except Error as e:
-            print(f"fail to connect with MySQL database: {e}")
-            return None
 app = Flask(__name__)
+
+def connect_to_database():
+    try:
+        connection = mysql.connector.connect(
+            host='127.0.0.1',
+            database='studentdb',
+            user='user',
+            password='user',
+            port='3309',
+            raise_on_warnings=True
+        )
+        if connection.is_connected():
+            print("Success! Connected to MySQL database.")
+            return connection
+    except Error as e:
+        print(f"Failed to connect to MySQL database: {e}")
+        return None
 
 @app.route('/')
 def index():
@@ -30,23 +27,22 @@ def index():
 
 @app.route('/attendance', methods=['POST'])
 def attendance():
-
-    # selected_date = request.form.get('selected_date')
-    # selected_date_obj = datetime.strptime(selected_date, '%Y-%m-%d')
-    # formatted_date = selected_date_obj.strftime('%Y-%m-%d')
+    journeyID = request.form.get('journeyID')
     connection = connect_to_database()
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM Attendance WHERE journey_id = 16")
-    attendance_data = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    if not attendance_data:
-        return render_template('index.html', no_data=True)
     
-    return render_template('index.html', attendance_data=attendance_data)
+    if connection:
+        cursor = connection.cursor()
+        query = "SELECT * FROM Attendance WHERE journey_id = %s"
+        cursor.execute(query, (journeyID,))
+        attendance_data = cursor.fetchall()
+        cursor.close()
+        connection.close()
+
+        if not attendance_data:
+            return render_template('index.html', no_data=True)
+        return render_template('index.html', attendance_data=attendance_data)
+
+    return render_template('index.html', no_data=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
