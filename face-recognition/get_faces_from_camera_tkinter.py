4ef207ca2e7475ab9fbdf0ad5f848f8687ce658a
GIANG NGUYỄN <hoangiang272003@gmail.com>
2024-11-16 18:27:51 +0700

    ...

--- face-recognition/get_faces_from_camera_tkinter.py
+++ face-recognition/get_faces_from_camera_tkinter.py
@@ -22,7 +22,6 @@ path_images_from_camera = "data/data_faces_from_camera/"
 #  Get face landmarks
 predictor = dlib.shape_predictor("data_dlib/shape_predictor_68_face_landmarks.dat")
 
-
 #  Use Dlib resnet50 model to get 128D face descriptor
 face_reco_model = dlib.face_recognition_model_v1("data_dlib/dlib_face_recognition_resnet_model_v1.dat")
 
